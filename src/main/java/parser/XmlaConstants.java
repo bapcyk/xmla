cbf8697ff7a41f4d3e7d46464c9ecf5e020bc251
me <me@me.me>
2022-03-02 21:16:54 +0200

    Renamed grammar file

---
+++ src/main/java/parser/XmlaConstants.java
@@ -0,0 +1,114 @@
+/*
+ * XmlaConstants.java
+ *
+ * THIS FILE HAS BEEN GENERATED AUTOMATICALLY. DO NOT EDIT!
+ *
+ * Permission is granted to copy this document verbatim in any
+ * medium, provided that this copyright notice is left intact.
+ *
+ * Copyright (c). All rights reserved.
+ */
+
+/**
+ * An interface with constants for the parser and tokenizer.
+ *
+ * @author   Bapcyk
+ * @version  1.0
+ */
+interface XmlaConstants {
+
+    /**
+     * A token identity constant.
+     */
+    public static final int WHITESPACE = 1001;
+
+    /**
+     * A token identity constant.
+     */
+    public static final int ATOM = 1002;
+
+    /**
+     * A token identity constant.
+     */
+    public static final int ATTRS_OPEN = 1003;
+
+    /**
+     * A token identity constant.
+     */
+    public static final int ATTRS_CLOSE = 1004;
+
+    /**
+     * A token identity constant.
+     */
+    public static final int TAG_OPEN = 1005;
+
+    /**
+     * A token identity constant.
+     */
+    public static final int TAG_CLOSE = 1006;
+
+    /**
+     * A token identity constant.
+     */
+    public static final int SINGLE_TAG = 1007;
+
+    /**
+     * A token identity constant.
+     */
+    public static final int LINE_TAG = 1008;
+
+    /**
+     * A token identity constant.
+     */
+    public static final int SPEC = 1009;
+
+    /**
+     * A token identity constant.
+     */
+    public static final int ATTR_STR = 1010;
+
+    /**
+     * A token identity constant.
+     */
+    public static final int QUOTE = 1011;
+
+    /**
+     * A token identity constant.
+     */
+    public static final int IS = 1012;
+
+    /**
+     * A token identity constant.
+     */
+    public static final int BLOCK = 1013;
+
+    /**
+     * A production node identity constant.
+     */
+    public static final int GO = 2001;
+
+    /**
+     * A production node identity constant.
+     */
+    public static final int ATTR = 2002;
+
+    /**
+     * A production node identity constant.
+     */
+    public static final int ATTRS = 2003;
+
+    /**
+     * A production node identity constant.
+     */
+    public static final int TAG = 2004;
+
+    /**
+     * A production node identity constant.
+     */
+    public static final int NODE = 2005;
+
+    /**
+     * A production node identity constant.
+     */
+    public static final int NODE_CONT = 2006;
+}
