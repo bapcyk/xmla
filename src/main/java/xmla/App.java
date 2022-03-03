b6dc5166505446a7ad85fd649d81a20fed30c337
me <me@me.me>
2022-03-03 19:08:01 +0200

    Changed output folder

--- src/main/java/xmla/App.java
+++ src/main/java/xmla/App.java
@@ -1,10 +1,29 @@
 package xmla;
 
+import parser.XmlaAnalyzer;
+
 
 public class App {
 
-	public static void main(String[] args) {
-		System.out.print("Hello world!");
+	public static int main(String[] args) {
+		if (2 != args.length) {
+			System.out.print("Syntax error: xmla|xml FILE");
+			return 1;
+		} else {
+			if ("xmla" == args[1]) {
+				System.out.print("AS XMLA");
+			} else if ("xml" == args[1]) {
+				System.out.print("AS XML");
+			}
+		}
+		return 0;
 	}
+	
+	
+
+}
+
+
+class ArithmeticCalculator extends XmlaAnalyzer {
 
 }
