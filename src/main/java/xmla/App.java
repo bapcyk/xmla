7653d0ecd8ddad56e3a362a2e43a76b0e7096e28
me <me@me.me>
2022-03-04 12:01:49 +0200

    Fixed string comparison

--- src/main/java/xmla/App.java
+++ src/main/java/xmla/App.java
@@ -15,69 +15,65 @@ import xmla.parser.XmlaParser;
 
 public class App {
 
-	private DocumentBuilder docBuilder;
-	private Document doc;
+        private DocumentBuilder docBuilder;
+        private Document doc;
 
-	App() throws ParserConfigurationException {
-		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-		docBuilder = docFactory.newDocumentBuilder();
-		doc = docBuilder.newDocument();
-	}
+        App() throws ParserConfigurationException {
+                DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+                docBuilder = docFactory.newDocumentBuilder();
+                doc = docBuilder.newDocument();
+        }
 
-	protected String dumpXml() throws TransformerException {
-		TransformerFactory transformerFactory = TransformerFactory.newInstance();
-		Transformer transformer = transformerFactory.newTransformer();
-		DOMSource source = new DOMSource(doc);
-		StreamResult result = new StreamResult(new StringWriter()); // new File("C:\\testing.xml"));
-		transformer.transform(source, result);
-		String xmlString = result.getWriter().toString();
-		return xmlString;
-	}
-	
-	protected void xmlaToXml() {
-		try {
-			XmlaParser parser = new XmlaParser(null, new ArithmeticCalculator());
-		}
-		catch (ParserCreationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+        protected String dumpXml() throws TransformerException {
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                DOMSource source = new DOMSource(doc);
+                StreamResult result = new StreamResult(new StringWriter()); // new File("C:\\testing.xml"));
+                transformer.transform(source, result);
+                String xmlString = result.getWriter().toString();
+                return xmlString;
+        }
 
-	protected void xmlToXmla() {
-	}
-	
-	public static int main(String[] args) {
-		if (2 != args.length) {
-			System.out.print("Syntax error: xmla|xml FILE");
-			return 1;
-		}
-		else {
-			try {
-				
-				if ("xmla" == args[1]) {
-					App app;
-					app = new App();
-					System.out.print(app.dumpXml());
-				}
-				else if ("xml" == args[1]) {
-					System.out.print("AS XML");
-				}
+        protected void xmlaToXml() {
+                try {
+                        XmlaParser parser = new XmlaParser(null, new ArithmeticCalculator());
+                } catch (ParserCreationException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                }
+        }
 
-			}
-			catch (ParserConfigurationException | TransformerException e) {
-				e.printStackTrace();
-			}
-		}
-		return 0;
-	}
+        protected void xmlToXmla() {
+        }
+
+        public static int main(String[] args) {
+                if (2 != args.length) {
+                        System.out.print("Syntax error: xmla|xml FILE");
+                        return 1;
+                } else {
+                        try {
+
+                                if (args[1].equals("xmla")) {
+                                        App app;
+                                        app = new App();
+                                        System.out.print(app.dumpXml());
+                                } else if (args[1].equals("xml")) {
+                                        System.out.print("AS XML");
+                                }
+
+                        } catch (ParserConfigurationException | TransformerException e) {
+                                e.printStackTrace();
+                        }
+                }
+                return 0;
+        }
 
 }
 
 class ArithmeticCalculator extends XmlaAnalyzer {
 
-	protected Node exitAtom(Token node) throws ParseException {
-		node.addValue(new String(node.getImage()));
-		return node;
-	}
+        protected Node exitAtom(Token node) throws ParseException {
+                node.addValue(new String(node.getImage()));
+                return node;
+        }
 }
