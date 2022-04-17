435ee3506ca37008f4d7186618217b9ee2b4a572
me <me@me.me>
2022-04-17 08:18:22 +0300

    Moved files, done right JAR

--- xmla.bat
+++ xmla.bat
@@ -1,1 +1,1 @@
-@%JAVA_HOME%/bin/java.exe --enable-preview -classpath target/xmla-0.0.1-SNAPSHOT-dep.jar xmla.App %*
+@%JAVA_HOME%/bin/java.exe --enable-preview -classpath target/xmla-0.0.1-SNAPSHOT-full.jar xmla.App %*
